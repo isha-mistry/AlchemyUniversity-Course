c8202a7a39c84c8c916ed9495c0e583a045469c6
isha-mistry <ishamistry.lamprostech@gmail.com>
2023-07-03 11:07:44 +0530

    week 5 task added

---
+++ Week 5/escrow-hardhat/app/src/App.js
@@ -0,0 +1,167 @@
+import { ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import deploy from './deploy';
+import {instanceEscrowContract} from './deploy';
+import Escrow from './Escrow';
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+export async function approve(escrowContract, signer) {
+  const approveTxn = await escrowContract.connect(signer).approve();
+  await approveTxn.wait();
+}
+
+function App() {
+  const [escrows, setEscrows] = useState([]);
+  const [account, setAccount] = useState();
+  const [signer, setSigner] = useState();
+
+  useEffect(() => {
+    async function getAccounts() {
+      const accounts = await provider.send('eth_requestAccounts', []);
+
+      setAccount(accounts[0]);
+      setSigner(provider.getSigner());
+    }
+
+    getAccounts();
+  }, [account]);
+
+  async function newContract() {
+    const beneficiary = document.getElementById('beneficiary').value;
+    const arbiter = document.getElementById('arbiter').value;
+    // const value = ethers.BigNumber.from(document.getElementById('wei').value);
+    let value = ethers.utils.parseUnits(document.getElementById('wei').value, "ether");
+    const escrowContract = await deploy(signer, arbiter, beneficiary, value);
+
+
+    const escrow = {
+      address: escrowContract.address,
+      arbiter,
+      beneficiary,
+      value: `${ethers.utils.formatEther(value.toString())}`,
+      handleApprove: async () => {
+        escrowContract.on('Approved', () => {
+          document.getElementById(escrowContract.address).className =
+            'complete';
+          document.getElementById(escrowContract.address).innerText =
+            "✓ It's been approved!";
+        });
+
+        await approve(escrowContract, signer);
+      },
+    };
+
+    const isIncludeEscrow = escrows.findIndex(escrowItem => escrowItem.address === escrow.address);
+
+    if (isIncludeEscrow === -1) {
+      setEscrows([...escrows, escrow]);
+    } else {
+      console.log('Escrow has already been inserted');
+    }
+  };
+
+  async function searchContract() {
+    console.log("hello");
+    const contractAddress = document.getElementById('contract').value;
+
+    if (contractAddress === "") return alert('contract address dont was provides')
+
+    const escrowContract = await instanceEscrowContract(contractAddress, signer);
+    console.log("escrowContract: ", escrowContract);
+
+    const valueContract = await Promise.all([escrowContract.arbiter(), escrowContract.beneficiary(), provider.getBalance(escrowContract.address)])
+    console.log("valueContract: ", valueContract);
+
+    const escrow = {
+      address: contractAddress,
+      arbiter: valueContract[0],
+      beneficiary: valueContract[1],
+      value: `${ethers.utils.formatEther(valueContract[2].toString())}`,
+      handleApprove: async () => {
+        escrowContract.on('Approved', () => {
+          document.getElementById(escrowContract.address).className =
+            'complete';
+          document.getElementById(escrowContract.address).innerText =
+            "✓ It's been approved!";
+        });
+
+        await approve(escrowContract, signer, valueContract[0]);
+      },
+    };
+
+    const isIncludeEscrow = escrows.findIndex(escrowItem => escrowItem.address === escrow.address);
+
+    if (isIncludeEscrow === -1){
+      setEscrows([...escrows, escrow]);
+    }else{
+      console.log('Escrow has already been inserted')
+    }
+  }
+
+  return (
+    <>
+      <div className="contract">
+        <h1> New Contract </h1>
+        <label>
+          Arbiter Address
+          <input type="text" id="arbiter" />
+        </label>
+
+        <label>
+          Beneficiary Address
+          <input type="text" id="beneficiary" />
+        </label>
+
+        <label>
+          Deposit Amount (in ETH)
+          <input type="text" id="wei" />
+        </label>
+
+        <div
+          className="button"
+          id="deploy"
+          onClick={(e) => {
+            e.preventDefault();
+
+            newContract();
+          }}
+        >
+          Deploy
+        </div>
+      </div>
+
+      <div className="contract">
+          <h1> SearchContract </h1>
+          <label>
+            Address Contract
+            <input type="text" id="contract" />
+          </label>
+
+          <div
+            className="button"
+            id="Search"
+            onClick={(e) => {
+              e.preventDefault();
+
+              searchContract();
+            }}
+          >
+            Search
+          </div>
+        </div>
+
+        <div className="existing-contracts">
+          <h1> Existing Contracts </h1>
+
+          <div id="container">
+            {escrows.map((escrow) => {
+              return <Escrow key={escrow.address} {...escrow} />;
+            })}
+          </div>
+        </div>
+    </>
+  );
+}
+
+export default App;
